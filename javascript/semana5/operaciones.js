3c12c795f2455638004540bdf8fd1a17ae21fa4e
rccardenas <rai.calderonc@gmail.com>
2022-11-05 15:24:51 -0500

    JS primeros retos

---
+++ javascript/semana5/operaciones.js
@@ -0,0 +1,19 @@
+// STRING, NUMBERS = tipos
+
+let n1 = 10; // number
+let n2 = 20; // number
+
+let verdadero = true; // booleam
+let falso = false; // booleam
+
+let nombre = "Rai"; // string
+
+let direccion = null 
+
+let suma = n1 + n2; // 30
+
+let resta = n1 - n2; // -10
+
+let producto = n1 * n2; // 200
+
+let division = n1 / n2; // 0.5
