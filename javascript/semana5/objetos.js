e737113061b1c35a0cdb8a9dc2ac5b8f7e206f71
rccardenas <rai.calderonc@gmail.com>
2022-11-07 11:01:42 -0500

    objetos por class

--- javascript/semana5/objetos.js
+++ javascript/semana5/objetos.js
@@ -97,3 +97,53 @@ function crearPersonas() {
 	personas.push(persona3);
 	console.log(personas);
 }
+
+// Creamos el MOLDE a partir del cual puedo crear multiples personas:
+class Person {
+	constructor(name, years, gender, height) {
+		this.name = name;
+		this.years = years;
+		this.gender = gender;
+		this.height = height;
+	}
+	is_adult() {
+		return this.years >= 18;
+	}
+	inches() {
+		return this.height / 2.54;
+	}
+	upper_name() {
+		return this.name.toUpperCase();
+	}
+	capital_name() {
+		return (
+			this.upper_name().substring(0, 1) +
+			this.name.substring(1, this.name.lenght)
+		);
+	}
+}
+
+// Funcion para crear varias personas a partir de 1 modelo:
+function crearPersonasClass() {
+	const persona1 = new Person("bruno", 32, "masculino", 179);
+	const persona2 = new Person("linder", 18, "masculino", 170);
+	const persona3 = new Person("elvia", 25, "femenino", 170);
+	const persona4 = new Person("arnold", 20, "masculino", 170);
+	personas.push(persona1);
+	personas.push(persona2);
+	personas.push(persona3);
+	personas.push(persona4);
+
+	console.log(personas);
+}
+
+function crearPersonasCustom() {
+	const nombre = prompt("Nombre: ");
+	const edad = prompt("Edad: ");
+	const genero = prompt("Genero: ");
+	const talla = prompt("Talla: ");
+	const persona = new Person(nombre, edad, genero, talla);
+	// Agregar a la nueva persona al arreglo "personas":
+	personas.push(persona);
+	console.log(personas);
+}
