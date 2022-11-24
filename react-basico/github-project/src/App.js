22adb26d75e727c05793af53806325717a1888d1
rccardenas <rai.calderonc@gmail.com>
2022-11-24 10:10:37 -0500

    Reto3->React/Microcomponente UserInformation

--- react-basico/github-project/src/App.js
+++ react-basico/github-project/src/App.js
@@ -1,12 +1,8 @@
 import { useState } from "react"; 
+import UserInformation from "./components/UserInformation";
 import './App.css';
-import hotel from "./assets/icons/hotel.png";
-import link from "./assets/icons/link.png";
-import pin from "./assets/icons/pin.png";
-import profile from "./assets/icons/profile.jpg";
 import search from "./assets/icons/search.png";
 import sun from "./assets/icons/sun.png";
-import twitter from "./assets/icons/twitter.png";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -44,45 +40,8 @@ function App() {
               <button className="btn-search" onClick={searchUser}>Search</button>
             </div>
         </div>
-        <div className="information-container">
-            <div className="image-container">
-                <img width="120" src={user?.avatar_url} alt=""/>
-            </div>
-            <div className="description-container">
-                <div className="user-date-container">
-                    <h2>{user?.name}</h2>
-                    <p>{user?.created_at}</p>
-                </div>
-                <div className="user-bio-container">
-                    <p>{user?.login}</p>
-                    <p>{user?.bio}</p>
-                </div>
-                <div className="card-information">
-                    <div>
-                        <h5>Repos</h5>
-                        <h2>{user?.public_repos}</h2>
-                    </div>
-                    <div>
-                        <h5>Follower</h5>
-                        <h2>{user?.followers}</h2>
-                    </div>
-                    <div>
-                        <h5>Following</h5>
-                        <h2>{user?.following}</h2>
-                    </div>
-                </div>
-                <div className="info-container">
-                    <div>
-                        <p><img width="15" src={pin} alt=""/>&nbsp;{user?.location}</p>
-                        <p><img width="15" src={twitter} alt=""/>&nbsp;{user?.twitter_username}</p>
-                    </div>
-                    <div>
-                        <p><img width="15" src={link} alt=""/>&nbsp;{user?.blog}</p>
-                        <p><img width="15" src={hotel} alt=""/>&nbsp;{user?.company}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        {/* AQUI HAREMOS UNA VALIDACION PARA QUE EL DIV SIGUIENTE EXISTA SIEMPRE Y CUANDO USER TENGA DATOS */}
+        { user && <UserInformation user={user}/>}
     </div>
   );
 }
