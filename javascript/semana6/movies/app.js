362a3e1a08d5368eebc4cb89804b549b7e3ef151
rccardenas <rai.calderonc@gmail.com>
2025-09-25 17:31:08 -0500

    reto catalogo peliculas

---
+++ javascript/semana6/movies/app.js
@@ -0,0 +1,19 @@
+import movies from "./data.json" with { type: "json" };
+
+const container = document.querySelector(".container");
+
+function readMovies() {
+  movies.entries.forEach((movie, index) => {
+    container.innerHTML += `<div class="movie">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+          index + 1
+        }.svg" alt="" />
+      <h4>${movie.title}</h4>
+      <p>
+        ${movie.description}
+      </p>
+    </div>`;
+  });
+}
+
+readMovies();
